02c5a6ea9ac659305ed643a8096d80cfa28614ef
jiyoung yu <jyjyu1204@gmail.com>
2022-11-01 12:15:47 +0900

    feature LOG change @RestController -> @Controller

--- src/main/java/org/shiftworks/controller/UserController.java
+++ src/main/java/org/shiftworks/controller/UserController.java
@@ -7,26 +7,44 @@ import org.shiftworks.domain.EmployeeVO;
 import org.shiftworks.domain.UserDTO;
 import org.shiftworks.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.log4j.Log4j;
 
-@RestController
+@Controller
 @RequestMapping("/login/*")
 @Log4j
 public class UserController {
 	@Autowired
 	private UserService user;
 	
-	@GetMapping("/customLogin")
-	public void loginGET() {
-		log.info("로그인 페이지.................");
+	
+	@GetMapping("/accessError")
+	public void  accessDenied(Authentication auth, Model model) {
+		log.info("access Denied: " + auth);
+		model.addAttribute("msg", "Access Denied");
 	}
+	
+	@GetMapping("/customLogin")//customLogin
+	public void loginInput(String error, String logout, Model model) {
+		log.info("error : "+error);
+		log.info("logout : "+logout);
 		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "Log out!!");
+		}
+	}
+	
 	@PostMapping("/customLogin")
 	public String loginPOST(HttpServletRequest request, UserDTO userDTO, RedirectAttributes rttr) throws Exception{
 		
@@ -42,6 +60,14 @@ public class UserController {
 		
 		return "redirect:/member";
 	}
+
+	
+	@GetMapping("/customLogout")
+	public void logoutGET() {
+		
+		log.info("user log out.........."  );
+	}
+	
 
 	
 }
